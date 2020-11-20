8d76e793eb5356c18d3671112ab61fae8d716557
Илья Зверев <izverev@logrocon.com>
2020-11-20 17:48:25 +0300

    gui shim + other small changes

--- xmplay/tools/chocolateyinstall.ps1
+++ xmplay/tools/chocolateyinstall.ps1
@@ -5,7 +5,12 @@ $packageArgs = @{}
 (ConvertFrom-Csv $meta -Delimiter ';').psobject.properties | ForEach-Object { $packageArgs[$_.Name] = $_.Value }
 
 $packageArgs["packageName"] = "$($env:ChocolateyPackageName)"
-$packageArgs["fileFullPath"] = "$(Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) $filename)"
-
+$packageArgs["unzipLocation"] = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage @packageArgs
+
+$exeMask = Join-Path $packageArgs["unzipLocation"] '*.exe'
+$exes = Get-ChildItem -Recurse $exeMask
+ForEach($exe in $exes){
+    New-Item -Path "$($exe).gui" -ItemType File | Out-Null
+}
