827be4d7e4060d6796b8c1581c2d8d423ad5bd98
Илья Зверев <izverev@logrocon.com>
2020-11-20 16:55:56 +0300

    export metadata to csv

--- xmplay/tools/chocolateyinstall.ps1
+++ xmplay/tools/chocolateyinstall.ps1
@@ -1,13 +1,11 @@
-﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://support.xmplay.com/files/20/xmplay383.zip'
+﻿
+$ErrorActionPreference = 'Stop';
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  url           = $url
-  softwareName  = 'xmplay*'
-  checksum      = '3FF935AB8B4486AB716AFEB9080F0C0C53C5B004F7C1C446860A5D45D465E9C7'
-  checksumType  = 'sha256'
-}
+$meta = Get-Content -Path "$($env:ChocolateyPackageFolder)\tools\packageArgs.csv" -Raw
+$packageArgs = @{}
+(ConvertFrom-Csv $meta -Delimiter ';').psobject.properties | ForEach-Object { $packageArgs[$_.Name] = $_.Value }
+
+$packageArgs["packageName"] = "$($env:ChocolateyPackageName)"
+$packageArgs["fileFullPath"] = "$(Join-Path (Split-Path -parent $MyInvocation.MyCommand.Definition) $filename)"
 
 Install-ChocolateyZipPackage @packageArgs
